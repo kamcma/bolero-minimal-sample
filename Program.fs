bcdd820bba493b7453893763e538a3d65f3cae98
Kyle McMahon <kyle.mcmahon@mac.com>
2020-03-22 17:29:16 +0000

    Upgrade to Blazor 3.2 preview

--- Program.fs
+++ Program.fs
@@ -1,17 +1,11 @@
 ﻿namespace BoleroMinimalSample
 
-open Microsoft.Extensions.DependencyInjection
-open Microsoft.AspNetCore.Components.Builder
-
-type Startup() =
-    member __.ConfigureServices(services: IServiceCollection) = ()
-
-    member __.Configure(app: IComponentsApplicationBuilder) = app.AddComponent<BoleroMinimalSample>("#app")
-
-open Microsoft.AspNetCore.Blazor.Hosting
+open Microsoft.AspNetCore.Components.WebAssembly.Hosting
 
 module Program =
     [<EntryPoint>]
     let main argv =
-        BlazorWebAssemblyHost.CreateDefaultBuilder().UseBlazorStartup<Startup>().Build().Run()
+        let builder = WebAssemblyHostBuilder.CreateDefault(argv)
+        builder.RootComponents.Add<BoleroMinimalSample>("#app") |> ignore
+        builder.Build().RunAsync() |> ignore
         0
