e722b6ee732413a0c70798e5dab447062baa9450
Kyle McMahon <kyle.mcmahon@mac.com>
2020-05-20 21:09:34 +0000

    Refactor app

--- Program.fs
+++ Program.fs
@@ -1,28 +1,64 @@
-﻿module Model =
-    type State =
-        { count: int }
+﻿module Counter =
+    open Elmish
+    open Bolero.Html
 
-    type Message =
+    type State =
+        { Count: int }
+
+    type Msg =
         | Increment
         | Decrement
 
-module View =
-    open Model
+    let init() =
+        { Count = 0 }, Cmd.none
+    
+    let update msg model =
+        match msg with
+        | Increment -> { model with Count = model.Count + 1 }, Cmd.none
+        | Decrement -> { model with Count = model.Count - 1 }, Cmd.none
+    
+    let view model dispatch =
+        div [] [
+            button [ on.click (fun _ -> dispatch Decrement) ] [ text "-" ]
+            text (string model.Count)
+            button [ on.click (fun _ -> dispatch Increment) ] [ text "+" ]
+        ]
+
+module MultiCounter =
+    open Elmish
     open Bolero.Html
 
-    let view model dispatch =
-        div []
-            [ button [ on.click (fun _ -> dispatch Decrement) ] [ text "-" ]
-              text (string model.count)
-              button [ on.click (fun _ -> dispatch Increment) ] [ text "+" ] ]
+    type State =
+        { Counter1: Counter.State
+          Counter2: Counter.State }
 
-module Update =
-    open Model
+    type Msg =
+    | Counter1 of Counter.Msg
+    | Counter2 of Counter.Msg
 
-    let update message model =
-        match message with
-        | Increment -> { model with count = model.count + 1 }
-        | Decrement -> { model with count = model.count - 1 }
+    let init() =
+        let counter1, counter1Cmd = Counter.init()
+        let counter2, counter2Cmd = Counter.init()
+        { Counter1 = counter1; Counter2 = counter2 },
+        Cmd.batch [
+            Cmd.map Counter1 counter1Cmd
+            Cmd.map Counter2 counter2Cmd
+        ]
+    
+    let update msg model =
+        match msg with
+        | Counter1 msg' ->
+            let res, cmd = Counter.update msg' model.Counter1
+            { model with Counter1 = res }, Cmd.map Counter1 cmd
+        | Counter2 msg' ->
+            let res, cmd = Counter.update msg' model.Counter2
+            { model with Counter2 = res }, Cmd.map Counter2 cmd
+    
+    let view model (dispatch: Msg -> unit) =
+        div [] [
+            Counter.view model.Counter1 (Counter1 >> dispatch)
+            Counter.view model.Counter2 (Counter2 >> dispatch)
+        ]
 
 
 module Program =
@@ -30,13 +66,9 @@ module Program =
     open Elmish
     open Bolero
 
-    open Model
-    open View
-    open Update
-
     type BoleroMinimalSample() =
-        inherit ProgramComponent<State, Message>()
-        override this.Program = Program.mkSimple (fun _ -> { count = 0 }) update view
+        inherit ProgramComponent<MultiCounter.State, MultiCounter.Msg>()
+        override this.Program = Program.mkProgram (fun _ -> MultiCounter.init()) MultiCounter.update MultiCounter.view
 
     [<EntryPoint>]
     let main argv =
